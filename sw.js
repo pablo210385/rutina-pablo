0f143a7dc9d75073a3f319cdc353f13dd54bd71a
pablo210385 <124082341+pablo210385@users.noreply.github.com>
2026-06-27 22:48:28 +0200

    feat: icono personalizado Rutina Diaria, SW v2

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "rutina-pablo-v2";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      response => response || fetch(event.request).catch(() => caches.match("./index.html"))
+    )
+  );
+});
